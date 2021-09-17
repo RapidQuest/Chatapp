d8108f879266a385a7f123c7ebaa381e8e866686
star-step <vanshajjha221@gmail.com>
2021-09-17 17:49:25 +0530

    updated join page

--- website/src/components/Join/Join.js
+++ website/src/components/Join/Join.js
@@ -9,8 +9,6 @@ const Join = () => {
   const [error, setError] = useState("")
   const { currentUser, logout } = useAuth()
   const history = useHistory()
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
 
   
   async function handleLogout() {
@@ -30,14 +28,8 @@ const Join = () => {
         <h1 className="heading">Join</h1>
           {error && <Alert variant="danger">{error}</Alert>}
           <strong className="text-light">Email:</strong> <p className="text-light">{currentUser.email}</p>
-        <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event)=>{setName(event.target.value)}}/>
-        </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event)=>{setRoom(event.target.value)}}/>
-        </div>
-        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/Chat?name=${name}&room=${room}`}>
-          <button className="button mt-20" type="submit">Sign In</button>
+        <Link to={'/Chat'}>
+          <button className="button mt-20" type="submit">Chat Now!</button>
         </Link>
       <div className="w-100 text-center mt-2">
         <Button variant="link" onClick={handleLogout}>
