3665419420260405f0557260f4df999794d1b016
Suhel Makkad <makkadsuhel11@gmail.com>
2021-11-12 11:52:08 +0530

    Revert "message getting received instantly from one side"
    This reverts commit 1c1973a7dec73cd642c94103b7dba09fbcf86ee5.

--- client/src/components/FullChat/index.js
+++ client/src/components/FullChat/index.js
@@ -58,10 +58,6 @@ export default function FullChat({ user, setSelectedUser, chats, setLastMessages
   useEffect(() => {
     socket = io(apiUrl, { transports: ["websocket"] });
     chats && socket.emit("join", chats.chatid);
-    
-  }, [user]);
-
-  useEffect(() => {
     socket.on("messageRecived", (message, userId, timeStamp, messageId) => {
       console.log("%cmessage recived", "color:red");
       setMessages((messages) => [
@@ -85,7 +81,7 @@ export default function FullChat({ user, setSelectedUser, chats, setLastMessages
         return lastMessages;
       });
     });
-  }, [])
+  }, [user]);
 
   const sendMessage = (event) => {
     event.preventDefault();
