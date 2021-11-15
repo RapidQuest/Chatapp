68a9120cf107a735174292daad053f7f03c62b4a
Suhel Makkad <makkadsuhel11@gmail.com>
2021-11-15 16:09:33 +0530

    cleaning server.js

--- server/index.js
+++ server/index.js
@@ -1,31 +1,19 @@
 const express = require("express");
-const socketio = require("socket.io");
-const http = require("http");
 const cors = require("cors");
+const socketio = require("socket.io");
 const jwt = require("jsonwebtoken");
-
-var current = new Date();
-
-// create express app
 const app = express();
 
+const routes = require("./router");
+const PORT = process.env.PORT || 5000;
 require("./db");
 
-// Import API route
-var routes = require("./router"); //importing route
-
-const PORT = process.env.PORT || 5000;
-// const router = require("./router");
-
 const bodyParser = require("body-parser");
-const { join } = require("path");
-
-const port = process.env.PORT || 5000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
+
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -43,19 +31,11 @@ app.use((req, res, next) => {
 
 routes(app);
 
-const server = app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// const app = express();
-// const server = http.createServer(app);
 const io = socketio(server);
-
-// app.use(cors());
-// app.use(router);
-// app.use(bodyParser.json());
-
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 io.on("connect", (socket) => {
   socket.on("join", (chatId) => {
@@ -64,20 +44,5 @@ io.on("connect", (socket) => {
 
   socket.on("sendMessage", (message, senderUserId, chatId, messageId) => {
     socket.to(chatId).emit("messageRecived", message, senderUserId, Date.now(), chatId, messageId);
-    // io.to(user.room).emit("message", { value: message, time: current.toLocaleString(), sentBy: user.name });
-    // io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
-
-    // callback();
   });
-
-  //   socket.on("disconnect", (socket) => {
-  //     const user = removeUser(socket.id);
-
-  //     if (user) {
-  //       io.to(user?.room).emit("message", { user: "Admin", text: `${user.name} has left.` });
-  //       io.to(user?.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
-  //     }
-  //   });
 });
-
-// server.listen(PORT, () => console.log(`Server has started on ${PORT}`));
