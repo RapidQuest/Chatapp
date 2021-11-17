3cfd6a8bebad0182fa90a89c2b1bbdba5b49acd1
star-step <vanshajjha221@gmail.com>
2021-11-17 19:05:18 +0530

    removed commented code

--- client/src/components/input/index.js
+++ client/src/components/input/index.js
@@ -19,30 +19,6 @@ const Input = ({ message, setMessage, sendMessage }) => (
           </span>
       </span>
   </div>
-  {/* <div className="input-group px-3 pt-3 mb-3">
-    <input
-      className="input form-control shadow-none"
-      type="text"
-      placeholder="Write a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={(event) => (event.key === "Enter" ? sendMessage(event) : null)}
-    />
-    <div className="input-group-prepend">
-      <span className="input-group-text bg-white rounded-0 h-100" id="basic-addon1">
-        <i className="fas fa-paperclip"></i>
-      </span>
-    </div>
-  </div>
-    <input
-      className="input form-control shadow-none"
-      type="text"
-      placeholder="Write a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={(event) => (event.key === "Enter" ? sendMessage(event) : null)}
-    />
-    <i class="fas fa-paperclip"></i> */}
     <button className="sendButton" onClick={(e) => sendMessage(e)}>
       Send
     </button>
